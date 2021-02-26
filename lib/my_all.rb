fcc078762ae3f68f3ef93ebbe368e7c7791d992f
sqnguyen820 <sqnguyen829@gmail.com>
2021-02-25 22:15:30 -0600

    done

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,24 @@
-require 'pry'
+# require 'pry'
+a = [1,2,3,4]
 
 def my_all?(collection)
+    if block_given?
+        i=0
+        values_returned = []
+        while i < collection.length
+            values_returned << yield(collection[i])
+            i += 1
+        end
+        # p values_returned
 
+        if values_returned.include?(false)
+            false
+        else
+            true
+        end
+    else
+        puts 'No block given!'
+    end
 end
+
+my_all?(a) {|x| x < 2}
